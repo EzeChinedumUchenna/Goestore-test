f7d3b8ac2a6522215e9a91caab859b582dc66fb8
Eze Chinedum <102483586+EzeChinedumUchenna@users.noreply.github.com>
2024-07-29 03:53:25 +0100

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /opt/tomcat/webapps
+pkill -f 'mvn jetty:run'  # Ensure any previous Jetty instance is stopped
 mvn jetty:run -Dovrdir=postgres -Pextjs,postgres > /var/log/geostore.log 2>&1 &
